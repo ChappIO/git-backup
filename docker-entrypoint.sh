3003553449fa30814672072ca4109d9acbf92f0b
Thomas Biesaart <thomas.biesaart@protonmail.com>
2022-04-23 18:05:39 +0200

    Fixes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -eu
 if [[ -z "${PUID-}" && -z "${PGID-}" ]]
 then
   # We are running through normal docker user changes, so nothing special to do
-  git-backup /git-backup "$@"
+  /git-backup "$@"
 else
   # We are running with an environment variable user change
   PUID=${PUID:-$(id -u)}
@@ -14,9 +14,9 @@ else
   groupmod -o -g "$PGID" git-backup
 
   # Own the backups folder
-  chown git-backup:git-backup /backup
+  chown git-backup:git-backup /backups
 
   # Let's go!
-  su -s /bin/sh /git-backup -c "/git-backup $@" whoami
+  su -s /bin/sh git-backup -c "/git-backup $@" whoami
 fi
 
